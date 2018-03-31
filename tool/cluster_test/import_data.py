3de5c776d56a65030de9f5f3fd419a5a2f554614
kungbob <kung.bob@gmail.com>
2018-03-31 17:52:21 +0800

    Modify import_data.py

--- tool/cluster_test/import_data.py
+++ tool/cluster_test/import_data.py
@@ -4,4 +4,12 @@ import pandas as pd
 
 df2 = pd.read_csv('program_code_python_only.csv')
 
-print(df2.head())
+# print(df2.head())
+
+# Iterate rows
+for index, row in df2.iterrows():
+    # print(row)
+    Problem_name = row.QCode
+    UserID = row.UserID
+    Solution_text = row.Solutions
+    print(Problem_name, UserID, Solution_text)
